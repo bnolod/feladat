f02e9f4a14196d02c08e9f03306c9f90d13d97d5
bnolod <sadfoolc@gmail.com>
2023-12-18 12:17:19 +0100

    3. feladat kesz (lista elemei kiiratasa vesszovel elvalasztva)

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ document.getElementById("add_num").addEventListener("click", function() {
   let num = document.getElementById("inp_num").value;
   numlist.push(num);
   document.getElementById("inp_num").value = "";
+  document.getElementById("list_num").innerHTML = numlist.join(", ");
 });
 
