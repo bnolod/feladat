6b658b459a0c1c4ccee5cd497b8f390bf4adcf90
bnolod <sadfoolc@gmail.com>
2023-12-18 12:23:35 +0100

    4. feladat (legkisebb szám kiiratása)

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ document.getElementById("add_num").addEventListener("click", function() {
   document.getElementById("list_num").innerHTML = numlist.join(", ");
 });
 
+
+document.getElementById("show_min").addEventListener("click", function() {
+    let min = Math.min( ...numlist);
+    document.getElementById("min_num").innerHTML = min;
+    });
+
